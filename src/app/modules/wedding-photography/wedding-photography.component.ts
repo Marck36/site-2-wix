7763639abee69c2a26a633fcb3087fdbe2ec4f2b
Marck36 <marcus.mariano.silva@gmail.com>
2025-03-07 12:24:24 -0300

    update angular 18 -> 19

--- src/app/modules/wedding-photography/wedding-photography.component.ts
+++ src/app/modules/wedding-photography/wedding-photography.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-wedding-photography',
-  templateUrl: './wedding-photography.component.html',
-  styleUrls: ['./wedding-photography.component.scss']
+    selector: 'app-wedding-photography',
+    templateUrl: './wedding-photography.component.html',
+    styleUrls: ['./wedding-photography.component.scss'],
+    standalone: false
 })
 export class WeddingPhotographyComponent {
 
